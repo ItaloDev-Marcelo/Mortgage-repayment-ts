3096769c6377981e42e37e1698f6dade8e233ea0
Italo Marcelo <Devfrontend2028study@gmail.com>
2025-09-12 14:58:06 -0300

    mais padding

--- src/components/RepaymentsEmpathy.tsx
+++ src/components/RepaymentsEmpathy.tsx
@@ -6,7 +6,7 @@ const RepaymentsEmpathy = () => {
              <img src={EmpathyIcon} alt='' />
         </figure>
        <h1 className=' text-white font-semibold text-[1.2em] mb-1'>Results shown here</h1>
-       <p className='mb-2 text-white opacity-90 '>Complete the form and click “calculate repayments” to see what 
+       <p className='mb-2 px-2 text-white opacity-90 '>Complete the form and click “calculate repayments” to see what 
   your monthly repayments would be.</p>
     </section> 
 
